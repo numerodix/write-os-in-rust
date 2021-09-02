cff736d924d2053b5718de6f20ecd7c4a6478291
Martin Matusiak <numerodix@gmail.com>
2021-09-02 20:12:50 +1000

    completed allocator design chapter

--- src/allocator.rs
+++ src/allocator.rs
@@ -7,9 +7,17 @@ use x86_64::{
     VirtAddr,
 };
 
-#[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+pub mod bump;
+pub mod fixed_size_block;
+use bump::BumpAllocator;
+use fixed_size_block::FixedSizeBlockAllocator;
 
+
+#[global_allocator]
+// static ALLOCATOR: LockedHeap = LockedHeap::empty();
+// static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(
+    FixedSizeBlockAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
@@ -41,4 +49,27 @@ pub fn init_heap(
     }
 
     Ok(())
+}
+
+
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
+
+
+/// Align the given address `addr` upwards to alignment `align`.
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
 }
