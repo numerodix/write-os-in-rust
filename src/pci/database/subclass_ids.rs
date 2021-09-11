2d64e77e6fd54703b8fac6a3f59f5ea2e6528f0d
Martin Matusiak <numerodix@gmail.com>
2021-09-11 19:14:50 +1000

    wip pci device listing

--- src/pci/database/subclass_ids.rs
+++ src/pci/database/subclass_ids.rs
@@ -1,3 +1,4 @@
+use alloc::{format, string::String};
 use enum_iterator::IntoEnumIterator;
 
 use super::class_ids::PCI_CLASS_ID;
@@ -6,4 +7,22 @@ use super::class_ids::PCI_CLASS_ID;
 pub enum PCI_SUBCLASS_UNCLASSIFIED {
     VGACompatibleUnclassifiedDevice = 0x00,
     NonVGACompatibleUnclassifiedDevice = 0x01,
+}
+
+#[derive(Debug, Clone, Copy, IntoEnumIterator, PartialEq, Eq)]
+pub enum PCI_SUBCLASS_MASS_STORAGE_CONTROLLER {
+    SCSIBusController = 0x00,
+    IDEController = 0x01,
+}
+
+pub fn get_class_name(class_id: u8, subclass_id: u8) -> Option<String> {
+    if class_id == PCI_CLASS_ID::Unclassified as u8 {
+        for id in PCI_SUBCLASS_UNCLASSIFIED::into_enum_iter() {
+            if subclass_id == id as u8 {
+                return Some(format!("{:?}", id));
+            }
+        }
+    }
+
+    None
 }
