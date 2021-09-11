467f62cc546bdb080de5804432bf1c0ead97ec73
Martin Matusiak <numerodix@gmail.com>
2021-09-11 19:27:39 +1000

    wip pci device listing

--- src/pci/database/subclass_ids.rs
+++ src/pci/database/subclass_ids.rs
@@ -1,26 +1,46 @@
 use alloc::{format, string::String};
 use enum_iterator::IntoEnumIterator;
 
-use super::class_ids::PCI_CLASS_ID;
-
 #[derive(Debug, Clone, Copy, IntoEnumIterator, PartialEq, Eq)]
-pub enum PCI_SUBCLASS_UNCLASSIFIED {
-    VGACompatibleUnclassifiedDevice = 0x00,
-    NonVGACompatibleUnclassifiedDevice = 0x01,
+#[repr(u16)]
+pub enum PCI_SUBCLASS_ID {
+    // Unclassified
+    VGACompatibleUnclassifiedDevice = 0x0000,
+    NonVGACompatibleUnclassifiedDevice = 0x0001,
+    // MassStorageController
+    SCSIBusController = 0x0100,
+    IDEController = 0x0101,
+    FloppyDiskController = 0x0102,
+    IPIBusController = 0x0103,
+    RADIController = 0x0104,
+    ATAController = 0x0105,
+    SerialATAController = 0x0106,
+    SerialAttachedSCSIController = 0x0107,
+    NonVolatileMemoryController = 0x0108,
+    // - Other = 0x0180,
+    // NetworkController
+    EthernetController = 0x0200,
+    TokenRingController = 0x0201,
+    FDDIController = 0x0202,
+    ATMController = 0x0203,
+    ISDNController = 0x0204,
+    WorldFipController = 0x0205,
+    PICMGController = 0x0206,
+    InfinibandController = 0x0207,
+    FabricController = 0x0208,
+    // - Other = 0x0280,
+    // DisplayController
+    VGACompatibleController = 0x0300,
+    XGAController = 0x0301,
+    ThreeDController = 0x0302,
+    // - Other = 0x0380,
 }
 
-#[derive(Debug, Clone, Copy, IntoEnumIterator, PartialEq, Eq)]
-pub enum PCI_SUBCLASS_MASS_STORAGE_CONTROLLER {
-    SCSIBusController = 0x00,
-    IDEController = 0x01,
-}
-
-pub fn get_class_name(class_id: u8, subclass_id: u8) -> Option<String> {
-    if class_id == PCI_CLASS_ID::Unclassified as u8 {
-        for id in PCI_SUBCLASS_UNCLASSIFIED::into_enum_iter() {
-            if subclass_id == id as u8 {
-                return Some(format!("{:?}", id));
-            }
+pub fn get_subclass_name(class_id: u8, subclass_id: u8) -> Option<String> {
+    for id in PCI_SUBCLASS_ID::into_enum_iter() {
+        let key: u16 = (class_id as u16) << 8 | (subclass_id as u16);
+        if key == id as u16 {
+            return Some(format!("{:?}", id));
         }
     }
 
--- src/pci/model.rs
+++ src/pci/model.rs
@@ -1,6 +1,7 @@
 use alloc::{format, string::String};
 
 use super::database::{class_ids::get_class_name, vendor_ids::get_vendor_name};
+use super::database::{subclass_ids::get_subclass_name};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct PciDevice {
@@ -31,6 +32,10 @@ impl PciDevice {
     pub fn class_name(&self) -> Option<String> {
         get_class_name(self.class)
     }
+
+    pub fn subclass_name(&self) -> Option<String> {
+        get_subclass_name(self.class, self.subclass)
+    }
 }
 
 impl PciDeviceBinding {
@@ -40,18 +45,23 @@ impl PciDeviceBinding {
             None => format!("0x{:x}", self.device.class),
         };
 
+        let subclass = match self.device.subclass_name() {
+            Some(name) => name,
+            None => format!("0x{:x}", self.device.subclass),
+        };
+
         let vendor = match self.device.vendor_name() {
             Some(name) => name,
             None => format!("vendor=0x{:x}", self.device.vendor),
         };
 
         format!(
-            "{:02x}:{:02x}.{:x} {}/0x{:x}: {}",
+            "{:02x}:{:02x}.{:x} {}/{}: {}",
             self.address.bus,
             self.address.device,
             self.address.function,
             class,
-            self.device.subclass,
+            subclass,
             vendor,
         )
     }
