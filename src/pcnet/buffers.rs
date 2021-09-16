7082e707bb83c9c14a7c60c888122355de3b1dee
Martin Matusiak <numerodix@gmail.com>
2021-09-16 19:52:10 +1000

    improve name

--- src/pcnet/buffers.rs
+++ src/pcnet/buffers.rs
@@ -100,7 +100,7 @@ type TransmitBuffers = PacketRingBuffer<NUM_TRANSMIT_BUFFERS>;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(packed)]
-pub struct InitStruct {
+pub struct InitBlock {
     // offset: 0
     mode: u16,
     rlen: u8,
@@ -127,7 +127,7 @@ pub struct InitStruct {
     tx_desc_phys_addr: u32,
 }
 
-impl InitStruct {
+impl InitBlock {
     fn new() -> Self {
         Self {
             mode: 0,
@@ -166,7 +166,7 @@ pub struct BufferManager {
     pub receive_descriptors: Box<ReceiveDescriptors>,
     pub transmit_descriptors: Box<TransmitDescriptors>,
 
-    init_struct: Box<InitStruct>,
+    init_block: Box<InitBlock>,
 }
 
 impl BufferManager {
@@ -180,7 +180,7 @@ impl BufferManager {
             receive_descriptors: Box::new(ReceiveDescriptors::new()),
             transmit_descriptors: Box::new(TransmitDescriptors::new()),
 
-            init_struct: Box::new(InitStruct::new()),
+            init_block: Box::new(InitBlock::new()),
         }
     }
 
@@ -221,7 +221,7 @@ impl BufferManager {
     }
 
     pub fn address_of_init_struct(&self) -> u32 {
-        let addr = &*self.init_struct as *const InitStruct as u64;
+        let addr = &*self.init_block as *const InitBlock as u64;
 
         self.translator.translate(addr)
     }
@@ -246,16 +246,16 @@ impl BufferManager {
         }
 
         // Initialize init struct
-        self.init_struct.mode = 0x0;
-        self.init_struct.set_rlen(NUM_RECEIVE_BUFFERS as u16);
-        self.init_struct.set_tlen(NUM_TRANSMIT_BUFFERS as u16);
-        self.init_struct.mac0 = mac[0];
-        self.init_struct.mac1 = mac[1];
-        self.init_struct.mac2 = mac[2];
-        self.init_struct.mac3 = mac[3];
-        self.init_struct.mac4 = mac[4];
-        self.init_struct.mac5 = mac[5];
-        self.init_struct.rx_desc_phys_addr = self.address_of_rx_descriptor(0);
-        self.init_struct.tx_desc_phys_addr = self.address_of_tx_descriptor(0);
+        self.init_block.mode = 0x0;
+        self.init_block.set_rlen(NUM_RECEIVE_BUFFERS as u16);
+        self.init_block.set_tlen(NUM_TRANSMIT_BUFFERS as u16);
+        self.init_block.mac0 = mac[0];
+        self.init_block.mac1 = mac[1];
+        self.init_block.mac2 = mac[2];
+        self.init_block.mac3 = mac[3];
+        self.init_block.mac4 = mac[4];
+        self.init_block.mac5 = mac[5];
+        self.init_block.rx_desc_phys_addr = self.address_of_rx_descriptor(0);
+        self.init_block.tx_desc_phys_addr = self.address_of_tx_descriptor(0);
     }
 }
